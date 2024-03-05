fba26ea392ba5f786dfcbb77ff2273d8bdd08bc0
Ezatullah-Mirzaie <158811113+Ezatullah-Mirzaie@users.noreply.github.com>
2024-03-05 03:05:04 -0800

    Add files via upload

---
+++ Circle.java
@@ -0,0 +1,24 @@
+package circle;
+class circle{
+    public double radius;
+    public double area(){
+        return Math.PI*radius*radius;
+    }
+    public double perimeter(){
+        return 2*Math.PI*radius;
+    }
+    public double circumference(){
+        return perimeter();
+    }
+}
+public class Circle {
+ 
+    public static void main(String[] args) {
+       circle c1=new circle();
+       c1.radius=7;
+       System.out.println(c1.area());
+       System.out.println(c1.perimeter());
+       System.out.println(c1.circumference());
+    }
+    
+}
